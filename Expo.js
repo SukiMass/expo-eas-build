2205c854a1c090c0940247d1c02c86c0e14eb12f
AdraProductStudio <adraforyou@gmail.com>
2024-07-26 16:49:21 +0530

    UPDATE : apk check

--- Expo.js
+++ Expo.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Expo() {
   return (
     <View style={styles.container}>
-      <Text>This is expo app using github actions with eas</Text>
+      <Text>This is expo app using github actions with eas hi</Text>
       <StatusBar style="auto" />
     </View>
   );
